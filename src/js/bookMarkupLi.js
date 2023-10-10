17bb664bec0363b643f541ee2be350379a684796
Kreal11 <kirillshyrokov2@gmail.com>
2023-10-10 14:32:33 +0300

    Виніс другу функцію розмітки з секції мейн для модульності
    Вона знаходиться в тому самому файлі, що і функція розмітки 1 книги (BookMarkupLi)

--- src/js/bookMarkupLi.js
+++ src/js/bookMarkupLi.js
@@ -1,7 +1,29 @@
+const booksList = document.querySelector('.bestsellers-list');
+
 export default function createBookMarkup(book) {
   return `<li class="book-card">
             <img class="book-card-img" src="${book.book_image}" alt="${book.description}" loading="lazy" width ="180" height ="256" />
             <h3 class="book-card-title">${book.title}</h3>
             <p class="book-card-text">${book.author}</p>
         </li>`;
+}
+
+export function createListMarkup(books) {
+  const lists = [];
+  for (let i = 0; i < books.length; i += 1) {
+    const liMarkup = books[i].books
+      .map((book, idx) => {
+        if (idx < 5) {
+          return createBookMarkup(book);
+        }
+      })
+      .join('');
+    const markup = `<ul class="top-category-list">
+    <h2 class="top-category-title">${books[i].list_name}</h2>
+    <div class="top-books-wrapper">${liMarkup}</div>
+  </ul>
+  <button class="book-card-btn" type="button">See more</button>`;
+    lists.push(markup);
+  }
+  booksList.innerHTML = lists.join('');
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import Notiflix from 'notiflix';
 import { BooksAPI } from './books-api';
-import createBookMarkup from './bookMarkupLi';
-const booksList = document.querySelector('.bestsellers-list');
+import { createListMarkup } from './bookMarkupLi';
+
 const booksApi = new BooksAPI();
 async function onPageLoad() {
   try {
@@ -12,22 +12,3 @@ async function onPageLoad() {
   }
 }
 onPageLoad();
-function createListMarkup(books) {
-  const lists = [];
-  for (let i = 0; i < books.length; i += 1) {
-    const liMarkup = books[i].books
-      .map((book, idx) => {
-        if (idx < 5) {
-          return createBookMarkup(book);
-        }
-      })
-      .join('');
-    const markup = `<ul class="top-category-list">
-    <h2 class="top-category-title">${books[i].list_name}</h2>
-    <div class="top-books-wrapper">${liMarkup}</div>
-  </ul>
-  <button class="book-card-btn" type="button">See more</button>`;
-    lists.push(markup);
-  }
-  booksList.innerHTML = lists.join('');
-}
