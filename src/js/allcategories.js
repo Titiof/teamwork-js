d95c6594b16cf12f7af4657d33e15cc80ee466c9
Uliana Nadorozhna <unadorozhna1@gmail.com>
2023-10-10 13:21:15 +0300

    added function

--- src/js/allcategories.js
+++ src/js/allcategories.js
@@ -2,10 +2,12 @@ import { BooksAPI } from "./books-api";
 import { menuTemplate } from "./allcategories/category_template";
 
 const listEl = document.querySelector('.vertical-menu');
+const activeEl = document.querySelector("[name='allcategories']");
 
 const booksAPI = new BooksAPI();
 
 window.addEventListener("DOMContentLoaded", async () => {
+<<<<<<< Updated upstream
     try{
         const listData =  await booksAPI.getCategoriesList();
         console.log(listData);
@@ -14,18 +16,42 @@ window.addEventListener("DOMContentLoaded", async () => {
     }catch(error) {
         Notiflix.Notify.failure('Oops, something went wrong!');
     }
+=======
+    const listData =  await fetchBookCategoryList();
+    //console.log(listData);
+    const markup = menuTemplate(listData);
+    
+    listEl.insertAdjacentHTML("beforeend", markup);
+
+    activeEl.classList.add('active');
+
+>>>>>>> Stashed changes
 });
 
 listEl.addEventListener('click', onChangeCategoryPage);
 
 async function onChangeCategoryPage(e){
     e.preventDefault();
+
+    if(e.target){
+        console.log(e.target);
+    }
     
     if(e.target.nodeName === "A"){
         console.log(e.target.name);
         const booksCategoryData = await booksAPI.getFullCategory(e.target.name);
         console.log(booksCategoryData);
+        if (listEl.firstChild.classList.contains('active')){
+            console.log(e.target);
+        } else{
+            activeEl.classList.remove('active');
+            e.target.classList.add('active')
+        }
+       
     }
 }
 
+function changeActiveElMenu(){
+
+}
 
