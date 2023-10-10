3014501fc3482aa90d5cc06da6a63778bcb80d4f
gektor558 <danil.trigub@gmail.com>
2023-10-10 17:33:30 +0200

    scroll-up

---
+++ src/js/scroll-up.js
@@ -0,0 +1,21 @@
+// id="home-link" айди кнопки
+
+// функция крутит страницу вверх
+function scrollToTop() {
+  document.body.scrollTop = 0; // Для Safari
+  document.documentElement.scrollTop = 0; // Для других
+}
+
+// скрыть или показывать кнопку в зависимости от положения
+window.onscroll = function () {
+  showScrollToTopButton();
+};
+
+function showScrollToTopButton() {
+  const scrollButton = document.getElementById("home-link");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+}
