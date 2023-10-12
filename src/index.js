f6204ee4224f85d4ad6fbd776eac696241e1457c
Rumberg Vitalii <modtitiof@gmail.com>
2023-10-12 10:55:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './js/header';
 import './js/support.js';
 import './js/main';
 import './js/allcategories.js';
+import './js/modal';
 import './js/clock';
 import './js/mobile-menu';
 import './js/scroll-up';
