48eab6af73c25ea5f18cc37023968ff3b347fee1
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-10-15 13:35:45 +0300

    update firebase

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './js/firebase';
 import './js/login/logout';
 import './js/dark-mode';
 import './js/allcategories';
---
+++ src/js/firebase.js
@@ -0,0 +1,94 @@
+import { initializeApp } from 'firebase/app';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendEmailVerification,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from 'firebase/auth';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBcETIWRzdvvBXoViQftnTvFFLpHkebOhw',
+  authDomain: 'book-bc5e2.firebaseapp.com',
+  projectId: 'book-bc5e2',
+  storageBucket: 'book-bc5e2.appspot.com',
+  messagingSenderId: '258455649332',
+  appId: '1:258455649332:web:4a54c5fd8564a412d79e61',
+  measurementId: 'G-2EMZHHRVM8',
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export async function signup(email, password, name) {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value
+    );
+
+    // якщо створено успішно
+
+    if (userCredential && auth.currentUser) {
+      sendEmailVerification(auth.currentUser);
+      updateProfile(auth.currentUser, {
+        displayName: name.value,
+        photoURL: 'https://picsum.photos/50/50?random=1',
+      });
+      Notify.success('Registration was successful!');
+
+      setTimeout(function () {
+        window.location.href = '/teamwork-js/';
+      }, 500);
+    }
+  } catch (error) {
+    if (error.code === 'auth/email-already-in-use') {
+      Notify.warning(
+        'This email address is already registered. Please go to the tab signin.'
+      );
+    } else {
+      Notify.error('An error occurred during registration.');
+    }
+  }
+}
+
+export async function signin(email, password) {
+  try {
+    //звертаємось до юзера (повертається url, name, email)
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value
+    );
+    const user = userCredential.user;
+    console.log(user); // повертається  url, name, email
+    refs.form.reset();
+    error = '';
+    Notify.success('Authorization was successful!');
+
+    setTimeout(function () {
+      window.location.href = '/teamwork-js/';
+    }, 500);
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    error = 'Invalid user email or password';
+    Notify.failure(error);
+  }
+}
+
+export function checkAuthState() {
+  auth.onAuthStateChanged(function (user) {
+    console.log('perevirka', user);
+
+    if (user) {
+      Notify.success(
+        `Successful authorized! You will be redirected to the home page`
+      );
+    } else {
+      Notify.info('Need authorized');
+    }
+  });
+}
