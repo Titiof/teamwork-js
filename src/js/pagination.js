174dc5751f41afa31a5837d386f7336a44618d5b
Rumberg Vitalii <modtitiof@gmail.com>
2023-10-15 23:06:30 +0300

    pagination is render function

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,26 +1,60 @@
-const windowWidth = Math.round(window.visualViewport.width);
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+import { booksTemplate } from './shopping-list';
+import { Notify } from 'notiflix';
 
-const visiblePages = windowWidth > 760 ? 3 : 5;
+const listBooksEl = document.querySelector('.shopping-list-selector');
 
-export const options = {
-  totalItems: 15,
-  itemsPerPage: 1,
-  visiblePages,
-  template: {
-    page: '<button type="button" class="tui-page-btn">{{page}}</button>',
-    currentPage:
-      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-    moveButton:
-      '<button type="button" class="tui-page-btn tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</button>',
-    disabledMoveButton:
-      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</span>',
-    moreButton:
-      '<button type="button" class="tui-page-btn tui-{{type}}-is-ellip">' +
-      '<span class="tui-ico-ellip">...</span>' +
-      '</button>',
-  },
+const books = JSON.parse(localStorage.getItem('shopping-list'));
+let currPage = 1;
+let itemsPerPage = 3;
+
+if (window.innerWidth < 768) {
+  itemsPerPage = 2;
+}
+
+const options = {
+  totalItems: books.length,
+  itemsPerPage: itemsPerPage,
+  visiblePages: itemsPerPage,
 };
+let pagination;
+
+export function resetPag(i) {
+  if (i <= 3) {
+    pagination = null;
+    try {
+      const p = document.querySelector('#tui-pagination-container');
+      p.remove();
+    } catch (err) {
+      if (i === 1) Notify.info(`There is one book in your shopping cart.`);
+      if (i === 2 || i === 3)
+        Notify.info(`There are ${i} books in your shopping cart.`);
+    }
+  } else pagination.setTotalItems(i);
+}
+
+//начальная проверка на необходимость пагинации
+export function isPagination() {
+  if (books.length > 3) {
+    listBooksEl.insertAdjacentHTML(
+      'afterend',
+      `<div id="tui-pagination-container" class="tui-pagination"></div>`
+    );
+    pagination = new Pagination('#tui-pagination-container', options);
+    document
+      .querySelector('#tui-pagination-container')
+      .addEventListener('click', () => {
+        if (pagination.getCurrentPage() !== currPage) {
+          currPage = pagination.getCurrentPage();
+          listBooksEl.innerHTML = booksTemplate(getPagArr());
+        }
+      });
+  }
+}
+
+function getPagArr() {
+  const books = JSON.parse(localStorage.getItem('shopping-list'));
+  let page = pagination.getCurrentPage();
+  return books.splice(page * 3 - 3, 3);
+}
